9f42c293b28fe2ccb60c9d3fa44996d26ae9b6e8
AdityaRaut <adityaraut6029@gmail.com>
2026-04-15 23:25:10 +0530

    Revert "setup of backend"
    This reverts commit ffedc390b193c3408cb5878698a47cfa2aeaece0.

--- validations/siteValidation.js
+++
@@ -1,8 +0,0 @@
-const { body } = require('express-validator');
-
-const siteValidation = [
-  body('name').trim().notEmpty().withMessage('Site name is required'),
-  body('address').trim().notEmpty().withMessage('Address is required'),
-];
-
-module.exports = { siteValidation };
